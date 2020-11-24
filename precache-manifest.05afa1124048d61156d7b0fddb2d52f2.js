9832a310e08c56dc3e0628ebd614c0149a59fc8f
MARIUS SWART <dswart54@yahoo.com>
2020-11-24 21:30:57 +0200

    Updates

---
+++ precache-manifest.05afa1124048d61156d7b0fddb2d52f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21c230f9e3f2acc8de0ea71558906c6a",
+    "url": "/client/index.html"
+  },
+  {
+    "revision": "9cb195fda7383701aa61",
+    "url": "/client/static/css/main.0b09de49.chunk.css"
+  },
+  {
+    "revision": "af29295afc6254e28c09",
+    "url": "/client/static/js/2.244a1352.chunk.js"
+  },
+  {
+    "revision": "1b028b1c651bfd0433f0b92684759e0a",
+    "url": "/client/static/js/2.244a1352.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9cb195fda7383701aa61",
+    "url": "/client/static/js/main.36008bad.chunk.js"
+  },
+  {
+    "revision": "7127e0582a9561966398",
+    "url": "/client/static/js/runtime-main.4fecf81f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/client/static/media/logo.5d5d9eef.svg"
+  }
+]);
